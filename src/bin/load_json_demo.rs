dd1cb147cf377b78d358b4007b0f3fed49962cdd
Gianna-Lynn <izyyzi@foxmail.com>
2026-03-21 17:49:44 +0800

    优化load_json_demo.rs和main.rs中的变量声明，移除不必要的可变性

--- src/bin/load_json_demo.rs
+++ src/bin/load_json_demo.rs
@@ -1,10 +1,9 @@
 use toy_vector_db::storage::inmemory::InMemoryVectorStore;
-use std::io::Error;
 fn main(){
 
     let store = InMemoryVectorStore::load("F://Repositories//Toy_Vector_DB//small_vectors.json").expect("加载失败");
     //尚不清楚这里的except用法.
-    let mut datas = store.records();
+    let datas = store.records();
     for data in datas{
         println!("{:?}",data);
     }
--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,6 @@ fn main(){
     store.insert(1, vec![1.0, 2.0, 3.0, 4.0]);
     store.insert(2, vec![2.0, 3.0, 4.0, 5.0]);
     store.insert(3, vec![3.0, 4.0, 5.0, 6.0]);
-    let mut result = search::flat::search_flat(&store, &vec![2.0, 3.0, 4.0, 5.0], 2);
+    let result = search::flat::search_flat(&store, &vec![2.0, 3.0, 4.0, 5.0], 2);
     print!("{:?}",result)
 }
